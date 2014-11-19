1f1eac1e2d470170b4a1be4c03ed5a51ea23aabc
Drew May <drewm135@gmail.com>
2014-11-19 14:21:38 -0800

    Revert "Uhhhhhhhh"
    This reverts commit 065b37775a989c2a33b0d1eb0f624bb649da386a.

--- In Class Exercise 3/Debug/subdir_rules.mk
+++ In Class Exercise 3/Debug/subdir_rules.mk
@@ -3,17 +3,17 @@
 ################################################################################
 
 # Each subdirectory must supply rules for building sources it contributes
-startup_ccs.obj: ../startup_ccs.c $(GEN_OPTS) $(GEN_HDRS)
+startup_ccs.obj: ../startup_ccs.c $(GEN_OPTS) $(GEN_SRCS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM Compiler'
-	"c:/ti/ccsv6/tools/compiler/arm_5.1.8/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 --abi=eabi -me -O2 --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.8/include" --include_path="C:/ti/TivaWare_C_Series-2.1.0.12573" -g --gcc --define=ccs="ccs" --define=PART_TM4C1294NCPDT --define=TARGET_IS_TM4C129_RA0 --display_error_number --diag_warning=225 --diag_wrap=off --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="startup_ccs.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv5/tools/compiler/arm_5.0.4/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 --abi=eabi -me -O2 -g --include_path="C:/ti/ccsv5/tools/compiler/arm_5.0.4/include" --include_path="C:/ti/TivaWare_C_Series-2.1.0.12573" --gcc --define=ccs="ccs" --define=PART_TM4C1294NCPDT --define=TARGET_IS_TM4C129_RA0 --diag_warning=225 --display_error_number --diag_wrap=off --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="startup_ccs.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-uart_echo_prompt.obj: ../uart_echo_prompt.c $(GEN_OPTS) $(GEN_HDRS)
+uart_echo_prompt.obj: ../uart_echo_prompt.c $(GEN_OPTS) $(GEN_SRCS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM Compiler'
-	"c:/ti/ccsv6/tools/compiler/arm_5.1.8/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 --abi=eabi -me -O2 --include_path="c:/ti/ccsv6/tools/compiler/arm_5.1.8/include" --include_path="C:/ti/TivaWare_C_Series-2.1.0.12573" -g --gcc --define=ccs="ccs" --define=PART_TM4C1294NCPDT --define=TARGET_IS_TM4C129_RA0 --display_error_number --diag_warning=225 --diag_wrap=off --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="uart_echo_prompt.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv5/tools/compiler/arm_5.0.4/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 --abi=eabi -me -O2 -g --include_path="C:/ti/ccsv5/tools/compiler/arm_5.0.4/include" --include_path="C:/ti/TivaWare_C_Series-2.1.0.12573" --gcc --define=ccs="ccs" --define=PART_TM4C1294NCPDT --define=TARGET_IS_TM4C129_RA0 --diag_warning=225 --display_error_number --diag_wrap=off --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="uart_echo_prompt.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- In Class Exercise 3/Debug/uart_echo_prompt.pp
+++ In Class Exercise 3/Debug/uart_echo_prompt.pp
@@ -1,12 +1,12 @@
 # FIXED
 
 uart_echo_prompt.obj: ../uart_echo_prompt.c
-uart_echo_prompt.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdint.h
-uart_echo_prompt.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdbool.h
-uart_echo_prompt.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/yvals.h
-uart_echo_prompt.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h
-uart_echo_prompt.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/linkage.h
-uart_echo_prompt.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/_lock.h
+uart_echo_prompt.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+uart_echo_prompt.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h
+uart_echo_prompt.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h
+uart_echo_prompt.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+uart_echo_prompt.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+uart_echo_prompt.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
 uart_echo_prompt.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_ints.h
 uart_echo_prompt.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h
 uart_echo_prompt.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/debug.h
@@ -19,12 +19,12 @@ uart_echo_prompt.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
 uart_echo_prompt.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h
 
 ../uart_echo_prompt.c: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdint.h: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdbool.h: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/yvals.h: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/linkage.h: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/_lock.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
 C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_ints.h: 
 C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h: 
 C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/debug.h: 
